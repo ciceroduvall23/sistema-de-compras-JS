b0dc9bddc4def120a3d6476a17cfbbf1fdc622c0
ciceroduvall23 <74693314+ciceroduvall23@users.noreply.github.com>
2023-01-26 16:01:01 -0300

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 if (document.readyState = "loading"){  /* readyState  vai detectar se a página html ainda está carregando ou se já foi carregada */
-    document.addEventListener("DOMContentLoaded", ready) /*addEventListener ouvidor de eventos */
+    document.addEventListener("DOMContentLoaded", ready) 
+  /*addEventListener ouvidor de eventos */
 }else{
 
-    ready()  /*se já foi carregada o javascript vai ser carregado */
+    ready() 
+   
+    /*se já foi carregada o javascript vai ser carregado */
 }
 
 function ready(){
@@ -15,46 +18,55 @@ function ready(){
     }
 
    const quantityInputs = document.getElementsByClassName("product-qtd-input") 
-   for (var i = 0; quantityInputs.length; i++){
+   for (var i = 0; i <  quantityInputs.length; i++){
     quantityInputs[i].addEventListener("change", updateTotal)
 }
-
 const addToCardButtons = document.getElementsByClassName("button-hover-background") 
-for (var i = 0; addToCardButtons.length; i++){
-    addToCardButtons[i].addEventListener("click", addProductToCard)
-}
+for (var i = 0; i <  addToCardButtons.length; i++){ /* vai percorrer por cada classe que tem o nome de button-hover-background*/
+    addToCardButtons[i].addEventListener("click", addProductToCard) /* adicionar o ouvir de evento addEventListener o evento vai ser o click*/
 
 }
+}
 
-function addProductToCard(event){
+function addProductToCard(event)
+{
     const button = event.target
     const productInfos = button.parentElement.parentElement
     const productImage = productInfos.getElementsByClassName("product-image")[0].src
     const productTitle = productInfos.getElementsByClassName("product-title")[0].innerText
     const productPrice = productInfos.getElementsByClassName("product-price")[0].innerText
-
+  
 
     let newCartProduct = document.createElement("tr")
     newCartProduct.classList.add("cart-product")
 
     newCartProduct.innerHTML =
-    `
-    <td class="product-identification">
-    <img src="${productImage}" alt="${productTitle}" class="cart-product-image">
-    <strong class="cart-product-title">${productTitle}</strong>
-  </td>
-  <td>
-    <span class="cart-product-price">${productPrice}</span>
-  </td>
-  <td>
-    <input type="number" value="1" min="0" class="product-qtd-input">
-    <button type="button" class="remove-product-button">Remover</button>
-  </td>
   `
+  <td class="product-identification">
+  <img src="${productImage}" alt="${productTitle}" class="cart-product-image">
+  <strong class="cart-product-title">${productTitle}</strong>
+</td>
+<td>
+  <span class="cart-product-price">${productPrice}</span>
+</td>
+<td>
+  <input type="number" value="1" min="0" class="product-qtd-input">
+  <button type="button" class="remove-product-button">Remover</button>
+</td>
+`
 
-  const tableBody = document.querySelector(".cart-table tbody")
-  tableBody.append(newCartProduct)
+const tableBody = document.querySelector(".cart-table tbody")
+tableBody.append(newCartProduct)
+
+updateTotal()
+newCartProduct.getElementsByClassName("product-qtd-input")[0].addEventListener("change",updateTotal)
+newCartProduct.getElementsByClassName("remove-product-button")[0].addEventListener("click", removeProduct)
+
 }
+
+
+
+
 
 
 function removeProduct(event){
@@ -81,5 +93,14 @@ function updateTotal(){
 }
 
 
+ /*
+ function addProductToCard(event){
+    const button = event.target
+    console.log(button)
 
+
+
+   
+}
+ */ 
 
